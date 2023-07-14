a74f1c8efede5d773eb324cdc5a68f9f766c576b
mbarrionuevo <matias_cba_99@hotmail.com>
2023-07-14 15:00:19 -0300

    Added useGetAnimals hook

---
+++ src/hooks/useGetAnimals.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { getAnimals } from 'services/animals';
+
+function useGetAnimals() {
+  const [data, setData] = useState([]);
+  const [problem, setProblem] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    const { data, problem } = await getAnimals();
+
+    let shuffledArray = [...data, ...data].sort(() => Math.random() - 0.9);
+
+    setProblem(problem);
+    setData(shuffledArray);
+
+    setIsLoading(false);
+  };
+
+  return [data, { problem, isLoading }];
+}
+
+export default useGetAnimals;
