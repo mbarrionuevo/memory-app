e2810137f75c0b9db5674b75c952c3e8c06a23dc
mbarrionuevo <matias_cba_99@hotmail.com>
2023-07-15 17:45:42 -0300

    Added a symbol '?' for cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,12 +8,16 @@ const Card = ({ imgUrl, title, flip, onClick }) => {
       } w-20 h-28 md:w-40 md:h-40 rounded-lg shadow transition-colors ease-in duration-300 hover:bg-slate-600`}
       onClick={onClick}
     >
-      {flip && (
+      {flip ? (
         <img
           className="object-cover rounded-lg w-full h-full"
           src={imgUrl}
           alt={title}
         />
+      ) : (
+        <div className="flex justify-center items-center h-full text-white text-5xl md:text-9xl">
+          ?
+        </div>
       )}
     </div>
   );
